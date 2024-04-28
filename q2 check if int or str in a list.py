27bf843571f487e2b8f5a078946a5d9da7e74f35
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-04-28 09:28:32 +0530

    Update and rename Q2 Check if int or str in a list.py to q2 check if int or str in a list.py

---
+++ q2 check if int or str in a list.py
@@ -0,0 +1,14 @@
+#Task: Write a Python code using the Lambda function to check every element of a list is an integer or string
+
+input_list =[1,3,'guvi', 56, 'python', 67, 'selenium', 9.0] 
+#Mixed input list with int and str
+
+#lamda function to execute the given condition
+result= lambda x: f"{x} is an integer" if type(x) is int\
+    else f"{x} is a string" if type(x) is str \
+    else f"{x} is not a defined data type"
+
+for x in input_list: 
+    #lamda function is called
+    print(result(x))
+
