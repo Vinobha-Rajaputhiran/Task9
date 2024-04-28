9be3d39bcef14aedce525813a6b8b28fb2e89b88
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-04-28 09:25:37 +0530

    Update and rename Q1 Output.py to q1 output.py

---
+++ q1 output.py
@@ -0,0 +1,9 @@
+#Task: What is the expected output of the following code:
+
+data =[10, 501, 22, 37, 100, 999, 87, 351]  
+#Program Given in the question
+result= filter(lambda x : x>4, data)
+print(list(result))
+
+
+#OUTPUT: [10, 501, 22, 37, 100, 999, 87, 351]
